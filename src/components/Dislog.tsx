01ef4d41caec9c212c9f9267ff945a947fb11120
Hentioe <meow.i5.br@gmail.com>
2025-09-17 01:27:57 +0800

    Implementing dialog

---
+++ src/components/Dislog.tsx
@@ -0,0 +1,68 @@
+import { Dialog } from "@ark-ui/solid/dialog";
+import { Accessor, JSX, Setter } from "solid-js";
+import { Portal } from "solid-js/web";
+
+interface Props {
+  open: Accessor<boolean>;
+  setOpen: Setter<boolean>;
+  title: string;
+  children: JSX.Element;
+  onConfirm?: () => Promise<boolean>;
+  onCancel?: () => void;
+}
+
+export default (props: Props) => {
+  const handleConfirm = async () => {
+    if (props.onConfirm) {
+      if (await props.onConfirm()) {
+        // 确定回调返回成功才关闭
+        props.setOpen(false);
+      }
+    } else {
+      props.setOpen(false);
+    }
+  };
+
+  const handleCancel = () => {
+    if (props.onCancel) {
+      props.onCancel();
+    }
+    props.setOpen(false);
+  };
+
+  return (
+    <Dialog.Root lazyMount unmountOnExit open={props.open()} onOpenChange={() => props.setOpen(false)}>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <div>
+              <Dialog.Title>{props.title}</Dialog.Title>
+              <Dialog.Description>{props.children}</Dialog.Description>
+            </div>
+            <div class="flex justify-end items-center">
+              <div class="flex gap-[2rem]">
+                <DialogButton onClick={handleCancel}>取消</DialogButton>
+                <DialogButton onClick={handleConfirm}>确认</DialogButton>
+              </div>
+            </div>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+};
+
+const DialogButton = (props: { children: JSX.Element; onClick?: () => void }) => {
+  const handleClick = () => {
+    if (props.onClick) {
+      props.onClick();
+    }
+  };
+
+  return (
+    <button onClick={handleClick} class="text-red-400">
+      {props.children}
+    </button>
+  );
+};
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,9 +1,12 @@
 import { Icon, IconifyIcon } from "@iconify-icon/solid";
 import { openUrl } from "@tauri-apps/plugin-opener";
-import { Show } from "solid-js";
+import { createSignal, Show } from "solid-js";
+import { Dialog } from "../components";
 import icons from "../icons";
 
 export default (props: { update?: Update }) => {
+  const [settingDialogOpen, setSettingDialogOpen] = createSignal(false);
+  const [volumeDialogOpen, setVolumeDialogOpen] = createSignal(false);
   const handleClickUpdate = async () => {
     const url = props.update?.download?.[0].url;
     if (url) {
@@ -14,24 +17,48 @@ export default (props: { update?: Update }) => {
   const Version = () => {
     return <p class="bg-white text-black text-base rounded-lg px-[0.5rem]">内测版</p>;
   };
+
+  const SettingDialog = () => {
+    return (
+      <Dialog title="设置" open={settingDialogOpen} setOpen={setSettingDialogOpen}>
+        我是设置弹窗
+      </Dialog>
+    );
+  };
+
+  const VolumeDialog = () => {
+    return (
+      <Dialog title="音量" open={volumeDialogOpen} setOpen={setVolumeDialogOpen}>
+        我是音量弹窗
+      </Dialog>
+    );
+  };
+
   return (
-    <header class="absolute top-[1rem] left-[1rem] right-[1rem] text-zinc-200 flex justify-between items-center">
-      <div class="flex-1">
-        <NavIcon icon="alarm-news" />
-      </div>
-      <Show when={props.update} fallback={<Version />}>
-        <p onClick={handleClickUpdate} class="bg-red-500 text-zinc-100 text-base rounded-lg px-[0.5rem] cursor-pointer">
-          发现新版本
-        </p>
-      </Show>
-      <div class="flex-1 flex gap-[1rem] items-center justify-end">
-        <NavIcon icon={icons.Volume} />
-        <NavIcon icon={icons.Setting} />
-      </div>
-    </header>
+    <>
+      <header class="absolute top-[1rem] left-[1rem] right-[1rem] text-zinc-200 flex justify-between items-center">
+        <div class="flex-1">
+          <NavIcon icon="alarm-news" />
+        </div>
+        <Show when={props.update} fallback={<Version />}>
+          <p
+            onClick={handleClickUpdate}
+            class="bg-red-500 text-zinc-100 text-base rounded-lg px-[0.5rem] cursor-pointer"
+          >
+            发现新版本
+          </p>
+        </Show>
+        <div class="flex-1 flex gap-[1rem] items-center justify-end">
+          <NavIcon icon={icons.Volume} onClick={() => setVolumeDialogOpen(true)} />
+          <NavIcon icon={icons.Setting} onClick={() => setSettingDialogOpen(true)} />
+        </div>
+      </header>
+      <SettingDialog />
+      <VolumeDialog />
+    </>
   );
 };
 
-const NavIcon = (props: { icon: string | IconifyIcon }) => {
-  return <Icon icon={props.icon} class="text-[1.5rem] h-[1.5rem] w-[1.5rem]" />;
+const NavIcon = (props: { icon: string | IconifyIcon; onClick?: () => void }) => {
+  return <Icon icon={props.icon} onClick={props.onClick} class="text-[1.5rem] h-[1.5rem] w-[1.5rem]" />;
 };
