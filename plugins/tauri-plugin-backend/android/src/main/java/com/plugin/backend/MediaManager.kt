f0375ac26824b2d49d25577bbcf1055a258853b2
Hentioe <meow.i5.br@gmail.com>
2025-09-30 16:58:07 +0800

    Optimized wind-strong sound loop

--- plugins/tauri-plugin-backend/android/src/main/java/com/plugin/backend/MediaManager.kt
+++ plugins/tauri-plugin-backend/android/src/main/java/com/plugin/backend/MediaManager.kt
@@ -21,7 +21,7 @@ enum class LocalMedia(
     TIMER("musics/timer.ogg", 0.0f, 0, true), // 计时器，循环音频
     RAIN("musics/rain.ogg", 0.0f, 0, true), // 雨声，循环音频
     RAIN_THUNDER("musics/rain-thunder.ogg", 0.0f, 0, true), // 雷雨，循环音频
-    WIND_STRONG("musics/wind-strong.ogg", 27f), // 强风，27 秒位置重叠
+    WIND_STRONG("musics/wind-strong.ogg", 0.0f, 0, true), // 强风，循环音频
     BONFIRE("musics/bonfire.ogg", 49f), // 篝火，49 秒位置重叠
     BEACH("musics/beach.ogg", 67f), // 海滩，67 秒位置重叠
     NATURE_STREAM("musics/nature-stream.ogg", 29f), // 自然（溪流），29 秒位置重叠
