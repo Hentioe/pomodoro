2d8c8c7213e6f0e945a3556e66e1456914c5d375
Hentioe <meow.i5.br@gmail.com>
2025-10-05 00:21:55 +0800

    3

--- src/dialogs/TodoDialog.tsx
+++ src/dialogs/TodoDialog.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify-icon/solid";
 import classNames from "classnames";
+import { createSignal, onMount } from "solid-js";
 import BasicDialog, { OpenProps } from "../components/BasicDialog";
 import CloseableTitleBar from "../components/CloseableTitleBar";
 
@@ -54,6 +55,24 @@ const Category = (props: { icon: string; label: string; colorScheme: string }) =
 };
 
 const TodoPreview = () => {
+  const [isActionButtonsShow, setIsActionButtonsShow] = createSignal(false);
+
+  let rootEl: HTMLDivElement | undefined;
+  let contentEl: HTMLDivElement | undefined;
+
+  const handleActionButtonsToggle = () => {
+    setIsActionButtonsShow(!isActionButtonsShow());
+  };
+
+  onMount(() => {
+    rootEl?.addEventListener("swiped-left", () => {
+      setIsActionButtonsShow(true);
+    });
+    rootEl?.addEventListener("swiped-right", () => {
+      setIsActionButtonsShow(false);
+    });
+  });
+
   const TimeTag = (props: { icon: string; label: string }) => {
     return (
       <div class="flex justify-center items-center gap-[0.15rem]">
@@ -64,19 +83,43 @@ const TodoPreview = () => {
   };
 
   return (
-    <div class="bg-blue-100 rounded-[2rem] px-[1rem] py-[0.5rem] flex items-center">
-      <div class="w-[2.25rem] h-[2.25rem] bg-white rounded-full flex justify-center items-center">
-        <div class="w-[1.2rem] h-[1.2rem] rounded-full border-2 border-blue-500" />
-      </div>
-      <div class="flex-1 px-[1rem]">
-        <div class="text-[0.9rem] text-gray-600 flex gap-[0.5rem]">
-          <TimeTag icon="solar:calendar-linear" label="今天" />
-          <TimeTag icon="tdesign:time" label="下午 4:00" />
+    <div ref={rootEl} class="relative h-[4rem] rounded-[2rem] overflow-hidden">
+      <div
+        ref={contentEl}
+        class={classNames([
+          "transition-all duration-100 absolute inset-0 bg-blue-100 px-[1rem] py-[0.5rem] flex items-center",
+          { "translate-x-[-16rem]": isActionButtonsShow() },
+        ])}
+      >
+        {/* 是否完成的圆 */}
+        <div class="w-[2.25rem] h-[2.25rem] bg-white rounded-full flex justify-center items-center">
+          <div class="w-[1.2rem] h-[1.2rem] rounded-full border-2 border-blue-500" />
         </div>
-        <p>完成 Todo 功能的开发</p>
+        {/* 内容 */}
+        <div class="flex-1 px-[1rem]">
+          <div class="text-[0.9rem] text-gray-600 flex gap-[0.5rem]">
+            <TimeTag icon="solar:calendar-linear" label="今天" />
+            <TimeTag icon="tdesign:time" label="下午 4:00" />
+          </div>
+          <p>完成 Todo 功能的开发</p>
+        </div>
+        {/* 更多按钮 */}
+        <div
+          onClick={handleActionButtonsToggle}
+          class={classNames([
+            "bg-white/90 w-[1.75rem] h-[1.75rem] rounded-full flex justify-center items-center",
+            { "bg-white/40!": isActionButtonsShow() },
+          ])}
+        >
+          <Icon icon="nrk:more" class="w-[1rem] text-zinc-400" />
+        </div>
       </div>
-      <div class="bg-white w-[1.75rem] h-[1.75rem] rounded-full flex justify-center items-center">
-        <Icon icon="uiw:more" class="w-[1rem] text-zinc-400" />
+      {/* 操作按钮 */}
+      <div class="flex absolute top-0 right-0 h-full z-[-1]">
+        <button class="w-[4rem] h-full text-white bg-blue-500">专注</button>
+        <button class="w-[4rem] h-full text-white bg-gray-500">上移</button>
+        <button class="w-[4rem] h-full text-white bg-green-500">编辑</button>
+        <button class="w-[4rem] h-full text-white bg-red-500">删除</button>
       </div>
     </div>
   );
