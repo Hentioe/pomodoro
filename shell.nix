c4d1a1090862c18b408de91ba97150b85a8c430f
Hentioe <meow.i5.br@gmail.com>
2025-10-24 05:56:01 +0800

    Update the Android NDK toolchain version

--- shell.nix
+++ shell.nix
@@ -45,19 +45,19 @@ stdenv.mkDerivation {
   ANDROID_NDK_ROOT = "${androidSdk}/libexec/android-sdk/ndk-bundle"; # Tauri 需要此变量
   ANDROID_NDK_HOME = "${androidSdk}/libexec/android-sdk/ndk-bundle"; # Rust 需要此变量
 
-  # NDK 构建工具版本
-  NDK_TOOLCHAINS_VERSION = "35";
+  # NDK 工具链目标 API
+  NDK_TOOLCHAINS_TARGET_API = "30"; # 此版本和 minSdk 保持一致
   # SDK 构建工具版本
   SDK_BUILD_TOOLS_VERSION = "36.0.0";
   # 修复前端网络故障
   GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules/";
 
-  # 指定工具位置
+  # 指定工具和参数
   shellHook = ''
-    export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android$NDK_TOOLCHAINS_VERSION-clang"
-    export CARGO_TARGET_AARCH64_LINUX_ANDROID_RUSTFLAGS="-L $ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/$NDK_TOOLCHAINS_VERSION"
-    export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android$NDK_TOOLCHAINS_VERSION-clang"
-    export CARGO_TARGET_X86_64_LINUX_ANDROID_RUSTFLAGS="-L $ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/x86_64-linux-android/$NDK_TOOLCHAINS_VERSION"
+    export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/clang"
+    export CARGO_TARGET_AARCH64_LINUX_ANDROID_RUSTFLAGS="-C link-arg=--target=aarch64-linux-android$NDK_TOOLCHAINS_TARGET_API"
+    export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/clang"
+    export CARGO_TARGET_X86_64_LINUX_ANDROID_RUSTFLAGS="-C link-arg=--target=x86_64-linux-android$NDK_TOOLCHAINS_TARGET_API"
     export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_SDK_ROOT/build-tools/$SDK_BUILD_TOOLS_VERSION/aapt2"
   '';
 }
